ab638be9cc6652487069263353f6a00709321fd1
djfcmf234 <djfcmf234@users.noreply.github.com>
2026-07-15 09:37:00 +0800

    Bust SW cache: v1 -> v2, add activate cleanup

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
-﻿const CACHE = "le-ge-bday-v1";
+const CACHE = "le-ge-bday-v2";
 const ASSETS = ["/le-ge-birthday/", "/le-ge-birthday/index.html", "/le-ge-birthday/manifest.json"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 
 self.addEventListener("fetch", e => {
